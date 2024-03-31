63298a08a6fc7bd31baa945f32e06dddafc3249c
Gael Montes <gaelmontesluna@gmail.com>
2024-03-30 21:43:47 -0500

    fixing contact form

---
+++ src/js/script.jsx
@@ -0,0 +1,88 @@
+const form = document.querySelector('form');
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+
+    const bodyMessage = `Nombre Completo: ${fullName.value}<br> Email: ${email.value}<br> Celular: ${phone.value}<br> Mensaje: ${mess.value}<br>`;
+
+    Email.send({
+        SecureToken: "449d1d84-aa5f-4d30-9313-c18849bff474",
+        To: "iquitostech@gmail.com",
+        From: "iquitostech@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "¡Enviado!",
+                    text: "Tu mensaje ha sido enviado, nos comunicaremos pronto para solucionar tus problemas.",
+                    icon: "success"
+                }); 
+            }
+        }
+    );
+}
+
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        })
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector(".error-txt.email")
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorTxtEmail.innerText = "Ingresa un correo válido";
+        } else {
+            errorTxtEmail.innerText = "El correo electrónico es necesario"
+        }
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if (!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")) {
+        sendEmail();
+    }
+})
