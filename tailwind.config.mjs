24bf976de5bcd5b7e63fc5219def67cca6e8b693
Gael Montes <gaelmontesluna@gmail.com>
2024-04-11 20:43:44 -0500

    fixing nav responsive design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const { nextui } = require("@nextui-org/react");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+],
 	darkMode: "class",
 	theme: {
 		extend: {
+			display: ['group-focus'],
+			opacity: ['group-focus'],
+			inset: ['grouo-focus'],
 			colors: {
-				main: "rgb(29 78 216)",
+				main: "rgb(26 71 196)",
 				gray: "#C6C6C6",
 				border: "#F1F8FF"
 			}
 		},
 	},
-	plugins: [require('@tailwindcss/forms')],
+	plugins: [require('@tailwindcss/forms'), nextui()],
 }
