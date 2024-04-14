1a57fad65fab73f0338f8e10eee044b714adc834
Gael Montes <gaelmontesluna@gmail.com>
2024-04-14 11:58:59 -0500

    fixing design for navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,14 +5,10 @@ const { nextui } = require("@nextui-org/react");
 export default {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
 ],
 	darkMode: "class",
 	theme: {
 		extend: {
-			display: ['group-focus'],
-			opacity: ['group-focus'],
-			inset: ['grouo-focus'],
 			colors: {
 				main: "rgb(26 71 196)",
 				gray: "#C6C6C6",
